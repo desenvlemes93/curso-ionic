b85c30af85e18bf268684a5faeb391cddc1ab20e
Ricardo Lemes <desenvlemes93@gmail.com>
2019-05-20 14:27:38 -0300

    1 parte ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,53 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage, MenuController } from 'ionic-angular';
+import { CredenciaisDTO } from '../../models/credencias.dto';
+import { AuthService } from '../../app/services/auth.service';
 
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  creds: CredenciaisDTO = {
+    email: "",
+    senha: ""
 
-  constructor(public navCtrl: NavController) {
+  };
+
+  constructor(public navCtrl: NavController,
+    public menu: MenuController,
+    public auth: AuthService) {
 
   }
 
+  ionViewWillEnter() {
+    this.menu.swipeEnable(false);
+  }
+
+  ionViewDidLeave() {
+    this.menu.swipeEnable(true);
+  }
+
+  ionViewDidEnter() {
+    this.auth.refreshToken()
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+        error => { });
+  }
+
+  login() {
+    this.auth.authenticate(this.creds)
+      .subscribe(response => {
+        this.auth.successfulLogin(response.headers.get('Authorization'));
+        this.navCtrl.setRoot('CategoriasPage');
+      },
+        error => { });
+  }
+  signup(){
+    console.log ('passou no signup')
+    this.navCtrl.push('SignupPage');
+  }
 }
